9722f2888699686244f202183cafd0fe83bb1b0c
park-hwaan <parkhwan1129@gmail.com>
2024-09-16 18:06:24 +0900

    메인 화면 디자인

--- app/src/main/java/com/example/fitee/MainActivity.kt
+++ app/src/main/java/com/example/fitee/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.fitee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.fitee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //glide 이미지 적용
+        Glide.with(this).load(R.drawable.mic_animaition).into(binding.gifImage)
     }
 }
